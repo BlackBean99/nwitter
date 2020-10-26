0a8e6f0722666c784152e64754223fcca2bb27bb
blackbean <ymecca12@gmail.com>
2020-10-26 10:14:51 +0900

    Updates

---
+++ precache-manifest.70e613a89d02de7d439311e49c173bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2127ada6e9b705bb15d3007893f2f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c698563fd8d27254a4b0",
+    "url": "/static/css/main.53813015.chunk.css"
+  },
+  {
+    "revision": "03ed6a6f60960f7c3a33",
+    "url": "/static/js/2.6fc6919f.chunk.js"
+  },
+  {
+    "revision": "73fe2f17e0b5f3735cb6289f8d13e5a5",
+    "url": "/static/js/2.6fc6919f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c698563fd8d27254a4b0",
+    "url": "/static/js/main.553e969f.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
